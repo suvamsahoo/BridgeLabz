68310b463c147d3b2cd12a60e1ee816237b2e2a8
suvamsahoo <suvamsahoo77@gmail.com>
2021-03-03 23:51:38 +0530

    Print true if day of month is between March 20 - June 20

---
+++ ShellScript/IfElse/DayMonth.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+read -p "Enter Date: " date
+read -p "Enter Month: " Month
+
+combo=0
+res="false"
+
+if [ $Month -ge 3 -a $Month -le 6 ]; then
+    # 30 for Apr, Jun
+    # 31 for Mar, May
+    dlimit=$((30 + ($Month % 2)))
+    if [ $date -ge 1 -a $date -le $dlimit ]; then
+        combo=$((($Month * 100) + $date))
+        # true Mar 20 to Jun 20
+        if [ $combo -ge 320 -a $combo -le 620 ]; then
+            res="true"
+        fi
+    fi
+fi
+
+echo "$Month/$date $res"
+
